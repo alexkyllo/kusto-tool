a78fc3b5eab9fd54be5ce3c1cd4cb08ff861e28d
Alex Kyllo <jekyllo@microsoft.com>
2022-01-20 10:31:14 -0800

    fix noxfile for pdm

--- docs/conf.py
+++ docs/conf.py
@@ -34,6 +34,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.doctest",
     "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -55,7 +56,8 @@ html_theme = "furo"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
+html_static_path = []
 
 
 # -- Extension configuration -------------------------------------------------
--- noxfile.py
+++ noxfile.py
@@ -5,18 +5,30 @@ from pathlib import Path
 import nox
 
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
+os.environ.update({"PDM_USE_VENV": "1"})
 
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def test(session):
     """Run the tests."""
     session.install("pytest")
-    session.run("python", "-m", "pytest", "tests/")
+    session.run("pdm", "install", "-G", "dev", external=True)
+    session.run("pdm", "run", "python", "--version")  # sanity check
+    session.run("pdm", "run", "pytest", external=True)
 
 
-@nox.session
+@nox.session(python="3.9")
+def coverage(session) -> None:
+    """Upload coverage data."""
+    session.install("coverage[toml]")
+    session.run("pdm", "install", "-G", "dev", external=True)
+    session.run("pdm", "run", "coverage", "report", "--fail-under=0", external=True)
+
+
+@nox.session(python="3.9")
 def docs(session):
     """Build the documentation."""
+    session.run("pdm", "install", "-G", "dev", external=True)
     args = session.posargs or ["-W", "-n", "docs", "docs/_build"]
 
     if session.interactive and not session.posargs:
@@ -29,6 +41,6 @@ def docs(session):
     session.install("-r", "docs/requirements.txt")
 
     if session.interactive:
-        session.run("sphinx-autobuild", *args)
+        session.run("pdm", "run", "sphinx-autobuild", *args, external=True)
     else:
-        session.run("sphinx-build", *args)
+        session.run("pdm", "run", "sphinx-build", *args, external=True)

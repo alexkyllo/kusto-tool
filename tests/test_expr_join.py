9885937d6a05a24cabc8f7945204735facec6f74
Alex Kyllo <jekyllo@microsoft.com>
2022-01-28 17:44:56 -0800

    add test for non-shuffle join

--- tests/test_expr_join.py
+++ tests/test_expr_join.py
@@ -11,13 +11,28 @@ def test_join_str():
     assert str(join == expected)
 
 
-def test_table_join():
+def test_table_shuffle_join():
     db = FakeDatabase("test", "testdb")
     t1 = TableExpr("table1", db, columns=dict(foo=str, bar=str, baz=int))
     t2 = TableExpr("table2", db, columns=dict(foo=str, bar=str, baz=int))
     join = t1.join(t2, on=["foo", "bar"], kind="inner", strategy="shuffle")
     expected = """cluster('test').database('testdb').['table1']
 | join kind=inner hint.strategy=shuffle (
+\tcluster('test').database('testdb').['table2']
+) on foo, bar
+"""
+    print(expected)
+    print(join)
+    assert str(join) == expected
+
+
+def test_table_shuffle_join():
+    db = FakeDatabase("test", "testdb")
+    t1 = TableExpr("table1", db, columns=dict(foo=str, bar=str, baz=int))
+    t2 = TableExpr("table2", db, columns=dict(foo=str, bar=str, baz=int))
+    join = t1.join(t2, on=["foo", "bar"], kind="inner")
+    expected = """cluster('test').database('testdb').['table1']
+| join kind=inner (
 \tcluster('test').database('testdb').['table2']
 ) on foo, bar
 """

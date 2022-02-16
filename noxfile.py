a211d01d5ef5a8d0477b7977ee19a908c70f357c
Alex Kyllo <alex.kyllo@gmail.com>
2022-02-16 01:13:00 -0800

    remove pdm stuff from noxfile

--- noxfile.py
+++ noxfile.py
@@ -4,15 +4,13 @@ from pathlib import Path
 
 import nox
 
-os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
-os.environ.update({"PDM_USE_VENV": "1"})
-
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+@nox.session(python=["3.8", "3.9", "3.10"])
 def test(session):
     """Run the tests."""
+    session.install(".")
     session.install("pytest")
     session.run("pytest")
 
@@ -20,15 +18,16 @@ def test(session):
 @nox.session(python="3.9")
 def coverage(session) -> None:
     """Upload coverage data."""
+    session.install(".")
     session.install("coverage[toml]")
-    session.run("pdm", "install", "-G", "dev", external=True)
-    session.run("pdm", "run", "coverage", "report", "--fail-under=0", external=True)
+    session.run("coverage", "report", "--fail-under=0")
 
 
 @nox.session(python="3.9")
 def docs(session):
     """Build the documentation."""
-    session.run("pdm", "install", "-G", "dev", external=True)
+    session.install(".")
+
     args = session.posargs or ["-W", "-n", "docs", "docs/_build"]
 
     if session.interactive and not session.posargs:
@@ -41,6 +40,6 @@ def docs(session):
     session.install("-r", "docs/requirements.txt")
 
     if session.interactive:
-        session.run("pdm", "run", "sphinx-autobuild", *args, external=True)
+        session.run("sphinx-autobuild", *args, external=True)
     else:
-        session.run("pdm", "run", "sphinx-build", *args, external=True)
+        session.run("sphinx-build", *args, external=True)
